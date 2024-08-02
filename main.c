dbad163bbc48dbae7eb2e781261adc83e4fd60ed
Emerson <eyenessus@gmail.com>
2024-08-02 11:24:38 -0300

    feature: implementação de calculadora em C

--- main.c
+++ main.c
@@ -1,5 +1,37 @@
 #include <stdio.h>
 
 int main() {
-  printf("Hello World!\n");
+  double num1, num2;
+  char op;
+
+  printf("Seja bem vindo ao Rooby\n");
+  printf("Rooby is an calculate\n");
+
+  printf("Digite o primeiro número: ");
+  scanf("%lf", &num1);
+
+  printf("Digite o operador: ");
+  scanf(" %c", &op);
+
+  printf("Digite o segundo número: ");
+  scanf("%lf", &num2);
+
+  switch (op) {
+    case '+':
+      printf("%f\n", num1 + num2);
+      break;
+    case '-':
+      printf("%f\n", num1 - num2);
+      break;
+    case '*':
+      printf("%f\n", num1 * num2);
+      break;
+    case '/':
+      printf("%f\n", num1 / num2);
+      break;
+    default:
+      printf("Operador inválido\n");
+      break;
+  }
+  
 }
